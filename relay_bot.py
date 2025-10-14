b7dbf03cfb1b2c3466af19eac921a3399af213bd
samtosuuu-arch <samtosuuu@gmail.com>
2025-10-14 15:53:35 +0330

    Update relay_bot.py

--- relay_bot.py
+++ relay_bot.py
@@ -1,39 +1,33 @@
 import asyncio
 from telethon import TelegramClient, events
-import os
+from telethon.sessions import StringSession
 
 # =======================
-# ⚡ تنظیمات خودتان
 API_ID = 23716374
 API_HASH = 'bca725880cba29c5dff697d093410ad5'
-PHONE = '+989213013798'  # شماره تلگرام شما با +98
-SESSION_NAME = 'relay_session'
 
-CHANNEL_SRC = 'dana_news'           # کانال مبدا بدون t.me/
-CHANNEL_DEST = 'dastavalkhabar'    # کانال مقصد بدون t.me/
+# جایگزین با رشته Session که گرفتیم
+SESSION_STRING = '1BJWap1sBuzAJNsjALm2JUy7QJPAf5fd4QPyetxZyh5wxAsxbAWa5BlxIdJdFL0FKTmsCoVpvRLR4nj3N9hfBJFkv8I1d-p79bcGVjHgEkThDq036EXeW8dqd2J7z_BFUgmaa4_mF8r5SVHtatQnAd1vOK7HT-OXP2_f7H0wEZ5060EFxloHSl8H4RwGU-VxfhiX7YuwwGJPd4lCJyW7bmoDC8ZufHZLzKj9LPW-BlgUUtENUiqlpiaMlgSKXfP6tmPqvZ-Dzn4LMNTgxmia19i3hAvYAtYaxcttPWQnN3RDN9nhPfHMTUvSQa_dmqWCCZ1OBT0XCfSZWSzsT1q0jPzSPW_ZmKQw='
+
+# کانال‌ها
+CHANNEL_SRC = 'dana_news'
+CHANNEL_DEST = 'dastavalkhabar'
 
 REMOVE_TEXT = "🆔 @Dana_News"
 ADD_TEXT = "💢@Dastavalkhabar"
 # =======================
 
-client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
+client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
 async def main():
-    await client.start(PHONE)
-    print("✅ ربات متصل شد و آماده دریافت پیام‌هاست.")
-
-    # دریافت کد تایید از کاربر
-    if not await client.is_user_authorized():
-        print("🔑 برای ادامه وارد کد تایید شوید.")
-        code = input('لطفاً کد تاییدی که دریافت کرده‌اید وارد کنید: ')  # وارد کردن دستی کد تایید
-        await client.sign_in(PHONE, code)
+    await client.start()
+    print("✅ Bot is connected and ready.")
 
     @client.on(events.NewMessage(chats=CHANNEL_SRC))
     async def handler(event):
         message = event.message
         text = message.message or ""
 
-        # حذف متن مبدا و اضافه کردن متن مقصد
         if REMOVE_TEXT in text:
             text = text.replace(REMOVE_TEXT, "")
         if ADD_TEXT not in text:
@@ -44,11 +38,10 @@ async def main():
                 await client.send_file(CHANNEL_DEST, message.media, caption=text)
             else:
                 await client.send_message(CHANNEL_DEST, text)
-            print(f"📨 پیام منتقل شد: {text[:30]}...")
+            print(f"📨 Message forwarded: {text[:30]}...")
         except Exception as e:
-            print(f"❌ خطا در ارسال پیام: {e}")
+            print(f"❌ Error sending message: {e}")
 
     await client.run_until_disconnected()
 
-# اجرا
 asyncio.run(main())
